3c25fdb1cb57744793863aa9e32b0d2a24cdbc6d
Gaurav suryavanshi <80798531+gauravsuryvanshi06@users.noreply.github.com>
2023-11-15 07:16:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,3 +42,24 @@ if st.button("Generate Bill"):
     else:
         st.error("Customer not found. Please check the mobile number.")
 
+# ... your existing Streamlit app code ...
+
+if st.button("Analyze Product Demand"):
+    sales_data = aggregate_product_sales(db)
+    demand_analysis = categorize_product_demand(sales_data)
+
+    # Display Demand Analysis
+    st.subheader("Demand Analysis")
+    for category, products in demand_analysis.items():
+        st.markdown(f"**{category.capitalize()} Demand Products:**")
+        st.write(products)
+    # ... existing demand analysis display code ...
+
+    # Display Sales Data in Table Format
+    st.subheader("Quantity-wise Selling of Products")
+    sales_df = pd.DataFrame(sales_data)
+    if not sales_df.empty:
+        sales_df.rename(columns={'_id': 'Product', 'total_sold': 'Total Sold'}, inplace=True)
+        st.table(sales_df)
+    else:
+        st.write("No sales data available.")
