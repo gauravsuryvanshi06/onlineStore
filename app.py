46889fdb4c8ccec46565e4e6f2dcf73bc0ca5859
Gaurav suryavanshi <80798531+gauravsuryvanshi06@users.noreply.github.com>
2023-11-15 09:04:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from database import get_database, add_customer, get_customer_by_mobile, add_product_to_customer
 from pdf import generate_pdf
+from database import aggregate_product_sales
 
 db = get_database()
 
