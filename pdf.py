c4b5fb30d71b2646536ff4b45beada17069079b2
Gaurav suryavanshi <80798531+gauravsuryvanshi06@users.noreply.github.com>
2023-11-15 07:11:25 +0530

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -1,16 +1,33 @@
+# pdf_generator.py
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
+from io import BytesIO
 
-def generate_pdf(customer, filepath='bill.pdf'):
-    c = canvas.Canvas(filepath, pagesize=letter)
+def generate_pdf(customer):
+    buffer = BytesIO()
+    c = canvas.Canvas(buffer, pagesize=letter)
     width, height = letter
-    c.drawString(100, height - 100, f"Customer Name: {customer['name']}")
-    c.drawString(100, height - 120, f"Mobile Number: {customer['mobile']}")
 
-    y_position = height - 140
+    # Ensure customer data is present
+    if not customer:
+        raise ValueError("No customer data provided")
+
+    # Customer Details
+    c.drawString(100, height - 100, f"Customer Name: {customer.get('name', 'N/A')}")
+    c.drawString(100, height - 120, f"Mobile Number: {customer.get('mobile', 'N/A')}")
+
+    # Product Details
+    y_position = height - 160
+    total_bill = 0
     for product in customer.get("products", []):
-        c.drawString(100, y_position, f"Product: {product['name']}, Price: {product['price']}")
+        product_details = f"{product['name']} - Quantity: {product.get('quantity', 1)} @ Price: {product['price']}"
+        c.drawString(100, y_position, product_details)
+        total_bill += product.get('quantity', 1) * product['price']
         y_position -= 20
 
-    c.drawString(100, y_position - 20, f"Total Bill: {sum(p['price'] for p in customer.get('products', []))}")
+    # Total Bill
+    c.drawString(100, y_position - 20, f"Total Bill: {total_bill}")
+
     c.save()
+    buffer.seek(0)
+    return buffer
